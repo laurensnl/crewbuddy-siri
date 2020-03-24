fcff3212d4b13f3de6b7a388fa1dfa427f06540b
Laurens Maarschalkerweerd <laurens@stafftraveler.com>
2020-03-24 22:59:22 +0100

    Update ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,11 @@ module.exports = {
       script: "./src/server.ts",
       instances: 1,
       autorestart: true,
-      watch: true,
+      watch: ["./src"],
+      watch_delay: 1000,
+      ignore_watch: ["node_modules"],
       max_memory_restart: "1G",
+      exp_backoff_restart_delay: 100,
       env: {
         NODE_ENV: "development"
       },
